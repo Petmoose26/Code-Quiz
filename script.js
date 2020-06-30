ced7becd21b7779e91dbbaeed36c61b18263669a
james louis <jlouis1026@gmail.com>
2020-06-30 15:53:50 -0400

    redid assignment

--- script.js
+++ script.js
@@ -1,130 +1,181 @@
-const startButton = document.getElementById('start-btn');
-const nextButton = document.getElementById('next-btn');
-const questionContainerElement = document.getElementById('question-container');
-const shuffledQuestions, CurrentQuestionsIndex;
-const questionElement = document.getElementById('question');
-const answerButtonElement = document.getElementById('answer-buttons');
+const MILLISECONDS_IN_DAY = 1000 * 60 * 60 * 24;
+const MILLISECONDS_IN_HOUR = 1000 * 60 * 60;
+const MILLISECONDS_IN_MINUTE = 1000 * 60;
 
+let score = 0;
+let selectedAnswer;
+let deadline;
 
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+let shuffledQuestions, currentQuestionsIndex;
+const questionElement = document.getElementById("question");
+const answerButtonElement = document.getElementById("answer-buttons");
+const scoreElement = document.getElementById("score");
+const scoreContainer = document.getElementById("score-container");
+const initialsElement = document.getElementById("initials");
 
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-    CurrentQuestionsIndex++
-    setNextQuestion()
-})
+const questions = [
+  {
+    question: "who was the first president of the USA?",
+    answer: [
+      { text: "George Washington", correct: true },
+      { text: "Will Smith", correct: false },
+      { text: "George Bush", correct: false },
+      { text: "Will Graham", correct: false },
+    ],
+  },
 
-function startGame() {
-    Console.log('started')
-    startButton.classList.add('hide')
-    shuffledQuestions = questionContainerElement.shadowRoot(() => Math.random() - .5)
-    CurrentQuestionsIndex = 0
-    questionContainerElement.remove('hide')
-    setNextQuestion
+  {
+    question: "What year was amazon founded?",
+    answer: [
+      { text: "1994", correct: true },
+      { text: "1998", correct: false },
+      { text: "1949", correct: false },
+      { text: "1995", correct: false },
+    ],
+  },
+
+  {
+    question: "Whats days does class start?",
+    answer: [
+      { text: "Tuesday, Thursday and Saturday", correct: true },
+      { text: "Tuesday, Thursday and Sunday", correct: false },
+      { text: "Monday, Wednesday and Saturday", correct: false },
+      { text: "Only Wednesday", correct: false },
+    ],
+  },
+
+  {
+    question: "Which is not a fruit?",
+    answer: [
+      { text: "Carrots", correct: true },
+      { text: "Strawberries", correct: false },
+      { text: "Blueberries", correct: false },
+      { text: "Raspberries", correct: false },
+    ],
+  },
+
+  {
+    question: "What year was Amazon founded?",
+    answer: [
+      { text: "1994", correct: true },
+      { text: "1900", correct: false },
+      { text: "1823", correct: false },
+      { text: "1949", correct: false },
+    ],
+  },
+];
+
+startButton.addEventListener("click", function () {
+  // THEN a timer starts and I am presented with a question.
+  startTimer();
+  currentQuestionsIndex = 0;
+  showQuestion();
+  scoreContainer.classList.add("hide");
+  score = 0;
+});
+
+var timerId;
+
+function startTimer() {
+  console.log("timer started");
+  // timer counts down from 3 minutes.
+  deadline = new Date().getTime() + MILLISECONDS_IN_MINUTE * 3;
+
+  timerId = setInterval(function () {
+    //var now is the current date and time in milliSeconds.
+    var now = new Date().getTime();
+    // var t is differnece bet the deadline and current time in milliSeconds
+    var t = deadline - now;
+
+    var minutes = Math.floor(
+      (t % MILLISECONDS_IN_HOUR) / MILLISECONDS_IN_MINUTE
+    );
+    var seconds = Math.floor((t % MILLISECONDS_IN_MINUTE) / 1000);
+    document.getElementById("countdown").innerHTML =
+      minutes + "m " + seconds + "s ";
+
+    if (t < 0) {
+      endGame();
+    }
+  }, 1000);
+  // when timer reaches zero. end quiz.
+}
+function showQuestion() {
+  console.log("show question");
+  //get the first question.
+  var questionAnswer = questions[currentQuestionsIndex];
+  // append question on page.
+  questionElement.innerText = questionAnswer.question;
+  questionContainerElement.classList.remove("hide");
+  answerButtonElement.children[0].innerText = questionAnswer.answer[0].text;
+  answerButtonElement.children[1].innerText = questionAnswer.answer[1].text;
+  answerButtonElement.children[2].innerText = questionAnswer.answer[2].text;
+  answerButtonElement.children[3].innerText = questionAnswer.answer[3].text;
+  startButton.classList.add("hide");
+  nextButton.classList.remove("hide");
 }
 
-function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[CurrentQuestionsIndex])
-    question.answer.forEach(answer => {
-        const button  = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonElement.appendChild(button)
+answerButtonElement.addEventListener("click", function (event) {
+  //check which child was clicked on
+  for (let i = 0; i < answerButtonElement.children.length; i++) {
+    var button = answerButtonElement.children[i];
+    if (button.innerText === event.target.innerText) {
+      selectedAnswer = questions[currentQuestionsIndex].answer[i];
     }
-    )
+  }
+  //get the answer first child
+  // set selected answer
+});
+
+nextButton.addEventListener("click", function (event) {
+  // the next question appears.
+  // if users answer === true, then update score.
+  console.log(selectedAnswer);
+  if (selectedAnswer.correct) {
+    score++;
+  } else {
+    // else subtract 30 secs from time.
+    deadline -= 30 * 1000;
+  }
+  // reset selected answer.
+  selectedAnswer = null;
+  currentQuestionsIndex++;
+  if (currentQuestionsIndex > questions.length - 1) {
+    endGame();
+    return;
+  }
+  showQuestion();
+});
+
+function endGame() {
+  //stop stimer.
+  clearInterval(timerId);
+  document.getElementById("countdown").innerHTML = "EXPIRED";
+  // show score.
+  scoreElement.innerHTML = score;
+  // save initials local storage.
+  scoreContainer.classList.remove("hide");
+  // hide questions and answers.
+  questionContainerElement.classList.add("hide");
+  // hide next button.
+  nextButton.classList.add("hide");
+  // show start button to play again.
+  startButton.classList.remove("hide");
 }
 
-function resetState() {
-    clearStatusClass(document.body)
-    setNextQuestion.button.classList.add('hide')
-    while (answerButtonElement.first) {
-        answerButtonElement.removeChild
-        (answerButtonElement.firstChild)
-    }
+function saveScore() {
+  var initials = initialsElement.value;
+  var player = {
+    initials: initials,
+    score: score,
+  };
+  localStorage.setItem("player score", JSON.stringify(player));
 }
 
-function showQuestion(question) {
-    questionElement.innerText = question.question;
-}
-
-function selectAnswer() { 
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonElement.children).forEach(button => {
-        setStatusClass(button.button.dataset.correct)
-    })
-    if(shuffledQuestions.length > CurrentQuestionsIndex + 1) {
-        nextButton.classList.remove('hide')
-    }else {
-        startButton.innerText = 'restart'
-        startButton.classList.remove('hide')
-    }
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
-    }
-}
-
-function clearStatusClass(element {
-    element.classList.add('correct')
-    element.classList.add('wrong')
-})
-
-const question = [
-    {
-        question: "who was the first president of the USA?",
-        answer: [
-        { text: "George Washington", correct: true},
-        { text: "Will Smith", correct:false}, 
-        { text: "George Bush", correct:false}, 
-        { text: "Will Graham", correct:false},
-        ]
-
-    }
-]
-
-const question = [
-    {
-        question: "What year was amazon founded?", 
-        answer: [
-            {text: "1994", correct: true}, 
-            {text: "1998", correct: false}, 
-            {text: "1949", correct: false},
-            {text: "1995", correct: false},
-        ]
-    }
-]
-
-const question = [
-    {
-        question: "Whats days does class start?", 
-        answer: [
-            {text: "Tuesday, Thursday and Saturday", correct: true}, 
-            {text: "Tuesday, Thursday and Sunday", correct: false}, 
-            {text: "Monday, Wednesday and Saturday", correct: false},
-            {text: "Only Wednesday", correct: false},
-        ]
-    }
-]
-
-const question = [
-    {
-        question: "Which is not a fruit?", 
-        answer: [
-            {text: "Carrots", correct: true}, 
-            {text: "Strawberries", correct: false}, 
-            {text: "Blueberries", correct: false},
-            {text: "Raspberries", correct: false},
-        ]
-    }
-]
+scoreContainer.addEventListener("submit", function (event) {
+  event.preventDefault();
+  saveScore();
+});
